49f768bc9e2adad58b09b06acc0bd96bd0ab9765
kazuki-komori <komokazu.nankai@gmail.com>
2020-03-21 17:15:41 +0900

    clean files

--- src/Options.ts
+++ src/Options.ts
@@ -1,5 +1,11 @@
 //機能一覧
 import DeleteMessage from "./option/DeleteMessage";
+import PageChangePost from "./option/PageChangePost";
+import PostPurchaseRequest from "./option/PostPurchaseRequest";
+import PostSearchResult from "./option/PostSearchResult";
 export {
     DeleteMessage,
+    PageChangePost,
+    PostPurchaseRequest,
+    PostSearchResult,
 }
---
+++ src/option/PageChangePost.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export default async ({blocks}:any, {user_id}:any)=>{
+    const url = "https://slack.com/api/chat.postEphemeral"
+    await axios.request({
+        headers:{
+            'authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`
+        },
+        url,
+        method: "POST",
+        data: {
+            channel: "tb-slack-library",
+            attachments: [
+                {
+                    "color": "#64B5F6",
+                    "blocks": blocks
+                }
+            ],
+            user: user_id
+        }
+    })
+}
