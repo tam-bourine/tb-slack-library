e2dc12c6af2ef19ef603e3b80ef08a05617924eb
kazuki-komori <komokazu.nankai@gmail.com>
2020-03-26 12:36:31 +0900

    マジックナンバー修正

--- src/views/ChangePage.ts
+++ src/views/ChangePage.ts
@@ -40,11 +40,12 @@ export default({data}:any, {key}:any, {page}:any) => {
             )
         }
     }
-    //検索結果を10件ずつに加工
-    if (blockKit.length>20 ){
-        let pages = Math.floor(blockKit.length/20)+1
+    //ブロックが20個以上あったときの処理
+    const splitNum:number = 20
+    if (blockKit.length>splitNum ){
+        let pages = Math.floor(blockKit.length/splitNum)+1
         let postResult:any = []
-        let i,j,temparray,chunk = 20;
+        let i,j,temparray,chunk = splitNum;
         for (i=0,j=blockKit.length; i<j; i+=chunk) {
             temparray = blockKit.slice(i,i+chunk);
             postResult.push(temparray)
--- src/views/ShowResult.ts
+++ src/views/ShowResult.ts
@@ -42,11 +42,12 @@ export default({data}:any) => {
             )
         }
     }
-
-    if (blockKit.length>20 ){
-        let pages = Math.floor(blockKit.length/20)+1
+    //ブロック数が20個を超えていたときの処理
+    const splitNum:number = 20
+    if (blockKit.length>splitNum ){
+        let pages = Math.floor(blockKit.length/splitNum)+1
         let postResult:any = []
-        let i,j,temparray,chunk = 20;
+        let i,j,temparray,chunk = splitNum;
         for (i=0,j=blockKit.length; i<j; i+=chunk) {
             temparray = blockKit.slice(i,i+chunk);
             postResult.push(temparray)
