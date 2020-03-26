a9a8b4ae97c7bb0b79c93c0864c246c6f2f09520
kazuki-komori <komokazu.nankai@gmail.com>
2020-03-26 21:45:49 +0900

    change page bug fix

--- src/Options.ts
+++ src/Options.ts
@@ -5,6 +5,7 @@ import PostSearchResult from "./options/PostSearchResult"; //検索結果表示
 import PostCompleate from "./options/PostCompleate"; //購入依頼完了メッセージ送信
 import PostFailed from "./options/PostFailed"; //購入依頼失敗メッセージ送信
 import PostRequestCancel from "./options/PostRequestCancel"; //購入依頼キャンセル時のメッセージ送信
+import PostChangePage from "./options/PostChangePage";
 export {
     DeleteMessage,
     PostPurchaseRequest,
@@ -12,4 +13,5 @@ export {
     PostCompleate,
     PostFailed,
     PostRequestCancel,
+    PostChangePage
 }
